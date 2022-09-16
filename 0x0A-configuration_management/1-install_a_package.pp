ef46b03015f12fec391aec2dbe453a88d011156e
Adejinmi Emmanuel <63516254+efdee1@users.noreply.github.com>
2022-09-17 00:50:00 +0100

    package install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Installs flask from pip3 using Puppet
-
-exec { 'install flask':
-  command => 'sudo pip3 install flask -v 2.1.0',
-  path    => '/usr/bin',
-  }
+# Install the puppet-lint package
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
+}
